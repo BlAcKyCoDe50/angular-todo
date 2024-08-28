46317fa57a1b5a4f0d282282db53dc66bf9467ef
Vishvajeet Singh Chouhan <117261834+BlAcKyCoDe50@users.noreply.github.com>
2024-08-28 18:40:57 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserListComponent } from './user-list/user-list.component';  // Import your UserListComponent
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , UserListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
